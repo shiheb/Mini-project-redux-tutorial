236b38449fdf3bc47c53d80b813a2bd058d26fa2
shiheb <chiheb.hmida@gmail.com>
2020-04-20 01:34:42 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import './App.css';
+
+import CakeContainer from './components/CakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+  
+
+      <CakeContainer /> 
+
+      <HooksCakeContainer />
+
+      <IceCreamContainer /> 
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
